94658e8fc6f7b1d412afff7f483b2d5555d07968
Caio Mori <caiomori22@gmail.com>
2026-05-20 17:29:05 -0300

    test: adiciona teste para o componente Card

--- tests/component/design-system/Card.spec.tsx
+++ tests/component/design-system/Card.spec.tsx
@@ -1,1 +1,17 @@
-export {};
+import { render } from '@testing-library/react-native';
+import React from 'react';
+
+import { Card } from '@/presentation/components/card/Card';
+import { Text } from '@/presentation/components/text/Text';
+
+describe('Card', () => {
+  it('renderiza conteúdo filho', () => {
+    const { getByText } = render(
+      <Card>
+        <Text>Conteúdo do card</Text>
+      </Card>,
+    );
+
+    expect(getByText('Conteúdo do card')).toBeTruthy();
+  });
+});
