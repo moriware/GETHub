95a10e0c9c8762866a58d86fd6d33e83c09fc8ae
Caio Mori <caiomori22@gmail.com>
2026-05-20 16:42:26 -0300

    refactor: adiciona suporte ao TanStack Query e atualiza dependências relacionadas

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,12 @@
 const { defineConfig, globalIgnores } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
 const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+const tanstackQueryEslintPlugin = require('@tanstack/eslint-plugin-query');
 
 module.exports = defineConfig([
   globalIgnores(['dist/*', '.expo/*', 'node_modules/*']),
   ...expoConfig,
+  ...tanstackQueryEslintPlugin.configs['flat/recommended'],
   eslintPluginPrettierRecommended,
   {
     rules: {
