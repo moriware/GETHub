6ca7a0f7a24b4da441b33a0bfe6d3149127e4cea
Caio Mori <caiomori22@gmail.com>
2026-05-20 15:28:20 -0300

    refactor: adiciona exportações dos componentes AppFlatList, AppPressable e AppView no índice de componentes primitivos

---
+++ src/presentation/components/primitives/index.ts
@@ -0,0 +1,3 @@
+export { AppFlatList } from '@/presentation/components/primitives/AppFlatList/AppFlatList';
+export { AppPressable } from '@/presentation/components/primitives/AppPressable/AppPressable';
+export { AppView } from '@/presentation/components/primitives/AppView/AppView';
