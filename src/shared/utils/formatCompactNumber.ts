6a091e8390818cc459e915c95832032351185a1c
Caio Mori <caiomori22@gmail.com>
2026-05-20 15:28:24 -0300

    refactor: atualiza a localidade para formatação de números compactos para pt-BR

--- src/shared/utils/formatCompactNumber.ts
+++ src/shared/utils/formatCompactNumber.ts
@@ -1,7 +1,7 @@
 import { UI_THRESHOLDS } from '@/shared/constants/ui';
 
 export function formatCompactNumber(value: number): string {
-  return new Intl.NumberFormat('en-US', {
+  return new Intl.NumberFormat('pt-BR', {
     notation: 'compact',
     maximumFractionDigits: UI_THRESHOLDS.compactNumberMaxFractionDigits,
   }).format(value);
